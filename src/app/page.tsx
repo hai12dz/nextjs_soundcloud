0966c2ab2b9f56705010461ff0b0dcbc5c811356
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-05-04 14:31:57 +0700

    add header homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import PrimarySearchAppBar from '@/components/header/header';
 import * as React from 'react';
 
 
 export default function HomePage() {
   return (
-    <div>
-      helo world
-    </div>
+    <>
+      <PrimarySearchAppBar />
+
+    </>
   );
 }
---
+++ src/app/playlist/page.tsx
@@ -0,0 +1,9 @@
+const PlaylistPage = () => {
+  return (
+    <div>
+      <h1>Playlist</h1>
+      <p>This is the playlist page.</p>
+    </div>
+  );
+}
+export default PlaylistPage;
