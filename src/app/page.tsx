5ec1e494b9c4d99695ac02539921b1804228b028
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-05-05 12:13:58 +0700

    feature slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import MainSlider from '@/components/main/main.slider';
+import { Container } from '@mui/material';
 import * as React from 'react';
 
 
 export default function HomePage() {
   return (
     <>
-      a
-
+      <Container>  <MainSlider />
+      </Container>
     </>
   );
 }
---
+++ src/components/main/main.slider.tsx
@@ -0,0 +1,40 @@
+"use client";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { Settings } from "react-slick"
+const MainSlider = () => {
+    var settings: Settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        nextArrow: <div className="arrow next">Next</div>,
+        prevArrow: <div className="arrow prev">Prev</div>,
+
+    };
+    return (
+        <Slider {...settings}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+            <div>
+                <h3>3</h3>
+            </div>
+            <div>
+                <h3>4</h3>
+            </div>
+            <div>
+                <h3>5</h3>
+            </div>
+            <div>
+                <h3>6</h3>
+            </div>
+        </Slider>
+    );
+}
+export default MainSlider;
