305072ed8bfe23afcac077417500254cdc902ad1
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-05-05 17:11:51 +0700

    wavesufer

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,4 +1,13 @@
-const DetailTrackPage = () => {
+"use client"
+import { useSearchParams } from "next/navigation"
+
+const DetailTrackPage = (props: any) => {
+    const searchParams = useSearchParams()
+    const { params } = props
+    const search = searchParams.get('search')
+    const slug = params.slug
+
+
     return (
         <div>
             <h1>Detail Track Page</h1>
