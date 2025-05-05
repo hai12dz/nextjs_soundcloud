ce2e233b16f7e3515c0e669702ea9c219bde4e39
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-05-05 16:12:45 +0700

    fetch api wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
 import MainSlider from '@/components/main/main.slider';
 import { Container } from '@mui/material';
 import * as React from 'react';
+import { sendRequestJS } from '@/utils/old.api';
+import { sendRequest } from '@/utils/api';
+
+export default async function HomePage() {
+  // const res = await fetch(`http://localhost:8000/api/v1/tracks/top`, {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //     category: "CHILL",
+  //     limit: 10,
+  //   }),
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   }
+  // })
+
+  // console.log("res", await res.json());
 
 
-export default function HomePage() {
+  const res = await sendRequest<IRequest>({
+    url: `http://localhost:8000/api/v1/tracks/top`,
+    method: 'POST',
+    body: {
+      category: "CHILL",
+      limit: 10,
+    }
+  })
   return (
     <>
-      <Container>  <MainSlider />
+      <Container>
+        <MainSlider />
+
       </Container>
     </>
   );
