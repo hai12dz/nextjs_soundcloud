9131933d4a086cba5a7a838acaa4e5a16949b838
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-05-16 17:07:05 +0700

    nested route

---
+++ src/app/(guest)/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+const SignInPage = () => {
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <p>Sign in to your account</p>
+            {/* Add your sign-in form here */}
+        </div>
+    );
+}
+export default SignInPage;
