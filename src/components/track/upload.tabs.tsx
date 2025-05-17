8878aad5744339d00701bed5e60ed32c9121cf37
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-05-17 22:05:26 +0700

    intergration upload file

--- src/components/track/upload.tabs.tsx
+++ src/components/track/upload.tabs.tsx
@@ -4,6 +4,7 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import * as React from 'react';
+import Step1 from './step1';
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -24,7 +25,7 @@ function CustomTabPanel(props: TabPanelProps) {
         >
             {value === index && (
                 <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
+                    <>{children}</>
                 </Box>
             )}
         </div>
@@ -47,19 +48,15 @@ const UploadTabs = () => {
                     onChange={handleChange}
                     aria-label="basic tabs example"
                 >
-                    <Tab label="Item One" />
-                    <Tab label="Item Two" />
-                    <Tab label="Item Three" />
+                    <Tab label="Tracks" />
+                    <Tab label="Basic Information" />
                 </Tabs>
             </Box>
             <CustomTabPanel value={value} index={0}>
-                hello
+                <Step1 />
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
                 Item Two
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={2}>
-                Item Three
             </CustomTabPanel>
         </Box>
     );
